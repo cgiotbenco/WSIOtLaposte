45aa45f6b163bf1cb199317f7d4bcef2363cb426
cgallard <c.gallard@laposte.net>
2015-12-29 14:23:56 +0100

    self.server
    self.server

--- server.js
+++ server.js
@@ -214,9 +214,9 @@ var SampleApp = function()
 
        // var express = require('express');
        // var app = express();
-        var server = require('http').Server(self.app);
-        self.io = require('socket.io').listen(server);
-        server.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
+        self.server = require('http').Server(self.app);
+        self.io = require('socket.io').listen(self.server);
+        self.server.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
 
 
 
